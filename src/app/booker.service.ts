76ce21ad4cce853588f939fee4028bd938ea28ad
nadias33 <salvadornadia240903@gmail.com>
2026-03-08 15:30:46 -0600

    Agregando endpoints Angular

--- src/app/booker.service.ts
+++ src/app/booker.service.ts
@@ -5,7 +5,7 @@ import { AuthRequest, AuthResponse, BookingPayload, BookingResponse } from './mo
 
 @Injectable({ providedIn: 'root' })
 export class BookerService {
-  
+
   // Headers obligatorios para Restful-Booker
   private headers = new HttpHeaders({
     'Content-Type': 'application/json',
@@ -14,28 +14,34 @@ export class BookerService {
 
   constructor(private http: HttpClient) {}
 
-  // Recibimos la 'baseUrl' desde el formulario antes de enviarla
+  // Endpoint 1: Crear Token
   createToken(baseUrl: string, payload: AuthRequest): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${baseUrl}/auth`, payload, { headers: this.headers });
   }
 
+  // Endpoint 2: Reserva estándar
   createStandardBooking(baseUrl: string, payload: BookingPayload): Observable<BookingResponse> {
     return this.http.post<BookingResponse>(`${baseUrl}/booking`, payload, { headers: this.headers });
   }
 
+  // Endpoint 3: Reserva VIP
   createVipBooking(baseUrl: string, payload: BookingPayload): Observable<BookingResponse> {
     return this.http.post<BookingResponse>(`${baseUrl}/booking`, payload, { headers: this.headers });
   }
 
+  // Endpoint 4: Reserva de Cumpleaños / Eventos
   createEventBooking(baseUrl: string, payload: BookingPayload): Observable<BookingResponse> {
     return this.http.post<BookingResponse>(`${baseUrl}/booking`, payload, { headers: this.headers });
   }
 
+  // Endpoint 5: Reserva de larga estancia
   createLongStayBooking(baseUrl: string, payload: BookingPayload): Observable<BookingResponse> {
     return this.http.post<BookingResponse>(`${baseUrl}/booking`, payload, { headers: this.headers });
   }
 
+  // Endpoint 6: Reserva para grupos
   createGroupBooking(baseUrl: string, payload: BookingPayload): Observable<BookingResponse> {
     return this.http.post<BookingResponse>(`${baseUrl}/booking`, payload, { headers: this.headers });
   }
+
 }
